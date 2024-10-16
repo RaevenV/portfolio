9915bf7564cebaee973993fd6c185ac0b30236a4
RaevenV <villarelr@gmail.com>
2024-10-16 22:01:36 +0700

    footer color

--- src/smallComponents/Footer.tsx
+++ src/smallComponents/Footer.tsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-t from-slate-800 to-slate-500 text-white text-center h-60 py-4 flex justify-center items-center flex-col font-bold font-raleway tracking-wide">
+    <footer className="bg-gradient-to-t bg-slate-800 text-white text-center h-60 py-4 flex justify-center items-center flex-col font-bold font-raleway tracking-wide">
       <div className="text-[20px] font-bold">Thank You!</div>
       <p className="text-[20px]">
         &copy; {new Date().getFullYear()} Raeven Villarel. All rights reserved.
